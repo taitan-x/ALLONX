e3443aa9ab8743b4fd325099ba22f3a5aa175d78
taitan-x <138150134+taitan-x@users.noreply.github.com>
2023-09-29 17:24:21 +0530

    Update start.py

--- AloneX/utils/inline/start.py
+++ AloneX/utils/inline/start.py
@@ -34,10 +34,12 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         [
             InlineKeyboardButton(text="Sᴜᴘᴘᴏʀᴛ", url=config.SUPPORT_GROUP),
             InlineKeyboardButton(text="Cʜᴀɴɴᴇʟ", url=config.SUPPORT_CHANNEL),
-        [
-               InlineKeyboardButton(text="Dᴇᴠᴇʟᴏᴘᴇʀ", user_id=OWNER),
-        ],
         ]
+        [
+            InlineKeyboardButton(text="Dᴇᴠᴇʟᴏᴘᴇʀ", user_id=OWNER),
+           )
+        ],
+        [
 
 close_key = InlineKeyboardMarkup(
             [
